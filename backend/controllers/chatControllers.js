b35abf8d6a9d0c3d9aab941ea7254d4741a5e2bd
Shantanu Swain <137885216+shantanusabyasachi16@users.noreply.github.com>
2024-08-30 11:05:46 +0530

    done the soket io

--- backend/controllers/chatControllers.js
+++ backend/controllers/chatControllers.js
@@ -30,9 +30,18 @@ export const sendMessage = async (req,res) => {
         await Promise.all([gotchat.save(), newMessage.save()]);
          
         // SOCKET IO
-       
-
-   //receive message
+        const receiverSocketId = getReceiverSocketId(receiverId);
+        if(receiverSocketId){
+            io.to(receiverSocketId).emit("newMessage", newMessage);
+        }
+        return res.status(201).json({
+            newMessage
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
+//receive message
 export const getMessage = async (req,res) => {
     try {
         const receiverId = req.params.id;
