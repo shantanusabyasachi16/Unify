6073d0c228451a0b54924bfb29b16ae44a4756f9
Shantanu Swain <137885216+shantanusabyasachi16@users.noreply.github.com>
2024-09-04 22:50:56 +0530

    displaying the selected user in chat box

--- frontend/src/chat/Message.jsx
+++ frontend/src/chat/Message.jsx
@@ -1,22 +1,24 @@
 import React from 'react';
 import SendInput from './SendInput';
 import ChatContainer from './ChatContainer';
+import { useSelector } from 'react-redux';
 
 const Message = () => {
+  const {selectedUsers} = useSelector(store=>store.user)
   return (
     <div className="flex flex-col h-full">
       <div className="flex gap-2 items-center bg-zinc-800 text-white px-4 py-2">
         <div className="avatar online">
           <div className="w-12 h-12 rounded-full overflow-hidden">
             <img 
-              src="https://th.bing.com/th/id/OIP.iApXL8G36uqoBd4khcqwRwHaHa?w=172&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7" 
-              alt="Shantanu's avatar" 
+              src={selectedUsers?.profilePhoto} 
+              alt="user profile" 
               className="w-full h-full object-cover"
             />
           </div>
         </div>
         <div className="flex flex-col flex-1">
-          <p>Shantanu</p>
+          <p>{selectedUsers?.fullName}</p>
         </div>
       </div>
       <ChatContainer />
