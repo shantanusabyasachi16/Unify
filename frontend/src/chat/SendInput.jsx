55fe47a4411f4305ed560a0d7ce7e7a9f788550a
Shantanu Swain <137885216+shantanusabyasachi16@users.noreply.github.com>
2024-09-12 22:26:18 +0530

    sending message

--- frontend/src/chat/SendInput.jsx
+++ frontend/src/chat/SendInput.jsx
@@ -1,16 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import axios from "axios";
+import { MESSAGE_API } from "@/utils/constant";
+import { useSelector } from "react-redux";
 
 const SendInput = () => {
+  const [input,setinput] = useState();
+ const {selectedUsers} = useSelector(store=>store.user)
+
+  const onSubmitHandler = async (e)=>{
+    e.preventDefault();
+    try {
+      const res = await axios.post(`${MESSAGE_API}/send/${selectedUsers?._id}`,input,{
+        headers:{
+          "Content-Type":'application/json'
+        },
+        withCredentials:true
+      })
+      console.log(res);
+      
+    } catch (error) {
+      console.log(error);
+      
+    }
+   
+  }
   return (
-    <form className="px-4 py-2 w-full flex items-center bg-gray-600">
+    <form onSubmit={onSubmitHandler} className="px-4 py-2 w-full flex items-center bg-gray-600">
       <Input
         type="text"
+        value={input}
+        onChange={(e)=>setinput(e.target.value)}
         placeholder="Send a message..."
         className="border text-sm rounded-lg flex-grow p-3 border-zinc-500 bg-gray-600 text-white"
       />
-      <Button
+      <Button type="submit"
         className="ml-2 text-sm text-white bg-gray-700 px-3 py-2 rounded-lg"
       >
         Send
