add9f63fac656921e91394e5a0eeab91aa98b1e7
Shantanu Swain <137885216+shantanusabyasachi16@users.noreply.github.com>
2024-09-16 13:26:49 +0530

    conditionally render the chat bubble

--- frontend/src/chat/Singlemessage.jsx
+++ frontend/src/chat/Singlemessage.jsx
@@ -1,25 +1,25 @@
 import React, { useEffect, useRef } from 'react';
+import { useSelector } from 'react-redux';
 
 const Singlemessage = ({ message }) => {
-  const messageRef = useRef(null); // Use messageRef instead of inputRef
-
+  const messageRef = useRef(null); 
+const {userInfo,selectedUsers} = useSelector(store=>store.user)
   useEffect(() => {
-    // Scroll to the message whenever it changes
+
     messageRef.current?.scrollIntoView({ behavior: "smooth" });
   }, [message]);
 
   return (
-    <div ref={messageRef} className="chat chat-end"> {/* Attach ref to message container */}
+    <div ref={messageRef} className={`chat ${message?.senderId === userInfo?._id ? 'chat-end' : 'chat-start'}`}>
       <div className="chat-image avatar">
         <div className="w-10 rounded-full">
           <img
             alt="User avatar"
-            src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
-          />
+            src={message?.senderId === userInfo?._id ? userInfo?.profilePhoto  : selectedUsers?.profilePhoto } />
         </div>
       </div>
       <div className="chat-header">
-        <time className="text-xs opacity-50 text-black-900">12:45</time>
+        <time className="text-xs  opacity-90 text-black-900">12:45</time>
       </div>
       <div className="chat-bubble">{message?.message}</div>
       <div className="chat-footer opacity-50"></div>
