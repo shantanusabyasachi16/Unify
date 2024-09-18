65f5684f666305f2b58893c7b90dc638440b0138
Shantanu Swain <137885216+shantanusabyasachi16@users.noreply.github.com>
2024-09-18 11:30:02 +0530

    implementing socket.io

---
+++ backend/SoketIo/socket.js
@@ -0,0 +1,25 @@
+import { Server } from "socket.io";
+import http from "http";
+import express from "express"
+
+
+const app = express();
+
+// to create the socket server
+
+const server = http.createServer(app);
+const io = new Server(server,{
+   cors:{
+    origin:['http://localhost:5173/'],
+    methods:['GET','POST']
+   } 
+});
+
+
+//to start the socket
+io.on('connection',(socket)=>{
+    console.log('user connected',socket.id);
+    
+})
+
+export {app,io,server}
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,9 @@ import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import Signup from "./auth/Signup";
 import Login from "./auth/login";
 import Home from "./chat/Home";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { io } from "socket.io-client";
 
 
 
@@ -22,7 +25,17 @@ const router = createBrowserRouter([
 
 ])
 function App() {
-  
+  const [socket, setsocket] = useState(null)
+  //user is authenticated or not
+  const {userInfo} = useSelector(store=>store.user)
+
+  useEffect(()=>{
+    if (userInfo) {
+      const socket = io("http://localhost:8000",{
+      })
+      setsocket(socket)
+    }
+  },[userInfo])
   return (
   <div>
      <RouterProvider router={router} />
