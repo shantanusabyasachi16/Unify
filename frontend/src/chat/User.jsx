3f81e7c63e26bdb5fa277258d352fdf60e17f499
Shantanu Swain <137885216+shantanusabyasachi16@users.noreply.github.com>
2024-09-19 09:17:37 +0530

    implemented the online user logic

--- frontend/src/chat/User.jsx
+++ frontend/src/chat/User.jsx
@@ -1,4 +1,3 @@
-
 import { setselectedUsers } from "@/redux/userSlice";
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
@@ -6,7 +5,10 @@ import { useDispatch, useSelector } from "react-redux";
 const User = (props) => {
   const user = props.user;
   const dispatch = useDispatch();
-  const selectedUsers = useSelector((state) => state.user.selectedUsers);
+  const { selectedUsers, onlineUsers } = useSelector(store => store.user);
+
+  // Convert IDs to strings for proper comparison
+  const online = onlineUsers.includes(user._id.toString());
 
   const UserHandler = () => {
     dispatch(setselectedUsers(user));
@@ -18,14 +20,14 @@ const User = (props) => {
         onClick={UserHandler}
         className={`flex gap-2 items-center text-white hover:text-zinc-900 hover:bg-zinc-200 rounded p-2 cursor-pointer ${selectedUsers?._id === user?._id ? 'bg-zinc-200 text-black' : ''}`}
       >
-        <div className="avatar online">
-        <div className="w-12 h-12 rounded-full overflow-hidden">
-    <img
-      src={user?.profilePhoto}
-      alt={`${user?.fullName}'s avatar`}
-      className="w-full h-full object-cover"
-    />
-  </div>
+        <div className={`avatar ${online ? 'online' : ''}`}>
+          <div className="w-12 h-12 rounded-full overflow-hidden">
+            <img
+              src={user?.profilePhoto}
+              alt={`${user?.fullName}'s avatar`}
+              className="w-full h-full object-cover"
+            />
+          </div>
         </div>
         <div className="flex flex-col flex-1">
           <div className="flex justify-between gap-2">
