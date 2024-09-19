509147032509cda4e532eae81ef4130a34e729de
Shantanu Swain <137885216+shantanusabyasachi16@users.noreply.github.com>
2024-09-19 21:20:08 +0530

    final debugging

--- frontend/src/chat/Message.jsx
+++ frontend/src/chat/Message.jsx
@@ -5,8 +5,9 @@ import { useDispatch, useSelector } from 'react-redux';
 import { setselectedUsers } from '@/redux/userSlice';
 
 const Message = () => {
-  const {selectedUsers,userInfo} = useSelector(store=>store.user)
+  const {selectedUsers,userInfo ,onlineUsers} = useSelector(store=>store.user)
   const dispatch = useDispatch();
+  const online = onlineUsers?.includes(selectedUsers?._id?.toString());
 //   useEffect(()=>{
 // return()=> dispatch(setselectedUsers(null));
 //   },[])
@@ -16,7 +17,7 @@ const Message = () => {
       selectedUsers != null ?(
 <div className="flex flex-col h-full">
       <div className="flex gap-2 items-center bg-zinc-800 text-white px-4 py-2">
-        <div className="avatar online">
+      <div className={`avatar ${online ? 'online' : ''}`}>
           <div className="w-12 h-12 rounded-full overflow-hidden">
             <img 
               src={selectedUsers?.profilePhoto} 
